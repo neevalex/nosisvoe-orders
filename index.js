74ec25954170c590708821677fc7fb1a1300dd2e
Neev Alex <neevalex@gmail.com>
2020-04-25 19:53:04 +0300

    dev

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const express = require('express');
 const app = express();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+
+db.defaults({ orders: [], user: {}, count: 0 })
+  .write()
+
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(express.static(__dirname + '/node_modules/jquery/dist'));
 
-app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/add', (req, res) => {
+    
+    if (req.query.name) {
+        console.log('Add: ');
+        console.log(req.query);
+        req.query.id = db.get('orders').value().length + 1;
+        req.query.date = Date.now();
+
+      db.get('orders').push(req.query).write();
+      res.sendStatus(200);
+    }
 });
+
+app.get('/get', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(db.get('orders').value());
+});
+
+
+
+app.get('/order', function (req, res) {
+
+  order = db.get('orders').find({ id: parseInt(req.query.id) }).value();
+
+  let date_ob = new Date(order.date);
+  let date = date_ob.getDate();
+  let month = date_ob.getMonth() + 1;
+  let year = date_ob.getFullYear();
+  
+  // prints date & time in YYYY-MM-DD format
+  formattedTime = year + "-" + month + "-" + date;
+
+  console.log(order);
+  res.render(__dirname + "/public/order.html", {order , date: formattedTime});
+
+});
+
+
 
 app.listen(3000, () => console.log('Gator app listening on port 3000!'));
--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,86 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-    $(".add-more").click(function(){ 
+    $(".add-more").click(function () {
         var html = $(".copy").html();
         $(".after-add-more").after(html);
     });
 
 
-    $("body").on("click",".remove",function(){ 
+    $("body").on("click", ".remove", function () {
         $(this).parents(".control-group").remove();
     });
+
+    function dropError(err) {
+        $('.err').text(err);
+        $('.err').show();
+    }
+
+
+    $(".add .submit").on("click", function (event) {
+        event.preventDefault();
+        let err = 0;
+        if ($(this).hasClass('added')) { return;}
+        data = $('form').serializeArray();
+        data.forEach((item) => {
+            if (item.value.length < 3) {
+                dropError('Пожалуйста заполните все поля!');
+                err = 1;
+            }
+        })
+
+        if (!err) {
+            $('.err').hide();
+
+            $.ajax({
+                    method: "get",
+                    url: "/add/",
+                    data
+                })
+                .done(function (msg) {
+                    $('.submit').text('Заказ успешно добавлен!'); 
+                    $('.submit').addClass('added');
+                });
+        }
+    });
+
+
+
+
+
+    if ( $( "body.orders" ).length ) {
+ 
+        console.log('orders');
+
+        $.ajax({
+            method: "get",
+            url: "/get/"
+        })
+        .done(function (html) {
+           // console.log(html);
+            i = 0;
+            html.forEach((order) => {
+                i++;
+                console.log(order.name);
+
+                let date_ob = new Date(order.date);
+                let date = date_ob.getDate();
+                let month = date_ob.getMonth() + 1;
+                let year = date_ob.getFullYear();
+                
+                // prints date & time in YYYY-MM-DD format
+                formattedTime = year + "-" + month + "-" + date;
+                let products = '';
+                order.product.forEach((prd) => {
+                    products += '<p>'+prd+'</p>';
+                })
+
+                var base = '<div class="order">' + formattedTime + ' <a class="title" data-toggle="collapse" href="#collapse' + i + '" role="button">' + order.name + '</a> '+order.phone+' '+order.total+'грн.'+' || Город:'+order.city+' THH:'+order.tnn+' || <a href="/order?id='+order.id+'">Ссылка поставщику</a><div class="collapse" id="collapse' + i + '"><div class="card card-body">'+products+'</div></div></div> ';
+                $(".container .orders").after(base);
+            })
+        });
+     
+    }
 
 
 });
